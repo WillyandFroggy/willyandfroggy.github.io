c6c8d568669b49c7d1e285338b9d110137aa0167
WillyandFroggy <willyandfroggy@gmail.com>
2023-11-15 22:21:35 -0500

    I'm working

--- script.js
+++ script.js
@@ -17,3 +17,18 @@ const menu = document.querySelector('.menu');
 menuButton.addEventListener('click', () => {
     menu.classList.toggle('active');
 });
+
+// Load ad text and hrefs
+const printsAdLink = document.querySelector('.print-ad-link');
+const tipJarLink = document.querySelector('.tip-jar-link');
+
+document.addEventListener("DOMContentLoaded", function() {
+  if (printsAdLink) {
+    printsAdLink.innerHTML = "BUY PRINTS";
+    printsAdLink.href = "#TEST1";
+  }
+  if (tipJarLink) {
+    tipJarLink.innerHTML = "TIP JAR: SUPPORT THE ARTIST";
+    tipJarLink.href = "#TEST2";
+  }
+});
