51667b9a8696a98955d44bf79786a59ba13b071f
WillyandFroggy <willyandfroggy@gmail.com>
2023-11-17 00:57:20 -0500

    Better get accustomed

--- script.js
+++ script.js
@@ -21,14 +21,19 @@ menuButton.addEventListener('click', () => {
 // Load ad text and hrefs
 const printsAdLink = document.querySelector('.print-ad-link');
 const tipJarLink = document.querySelector('.tip-jar-link');
+const footerCopyright = document.getElementById('footer-copyright');
+const currentYear = new Date().getFullYear();
 
 document.addEventListener("DOMContentLoaded", function() {
+  if (footerCopyright) {
+    footerCopyright.innerHTML = "All Content &#169; 2017-" + currentYear + " Amari &#268;ertkus";
+  }
   if (printsAdLink) {
     printsAdLink.innerHTML = "BUY PRINTS";
     printsAdLink.href = "#TEST1";
   }
   if (tipJarLink) {
-    tipJarLink.innerHTML = "TIP JAR: SUPPORT THE ARTIST";
+    tipJarLink.innerHTML = "TIP THE ARTIST";
     tipJarLink.href = "#TEST2";
   }
 });
