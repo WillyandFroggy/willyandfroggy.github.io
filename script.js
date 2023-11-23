4b2c854a180e9b5749786e6280c69c584b67d891
WillyandFroggy <willyandfroggy@gmail.com>
2023-11-22 21:04:58 -0500

    I don't mean any harm by going there

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// Variable to update
+const latestComic = "003";
+
 // Sticky header
 var prevScrollpos = window.scrollY;
 window.onscroll = function() {
@@ -23,6 +26,18 @@ const printsAdLink = document.querySelector('.print-ad-link');
 const tipJarLink = document.querySelector('.tip-jar-link');
 const footerCopyright = document.getElementById('footer-copyright');
 const currentYear = new Date().getFullYear();
+const rightArrows = document.querySelectorAll('.right-arrows-navigate-comic');
+const leftArrows = document.querySelectorAll('.left-arrows-navigate-comic');
+const nextArrow = document.getElementById("next-arrow");
+const latestArrow = document.getElementById("latest-arrow");
+const firstArrow = document.getElementById("first-arrow");
+const previousArrow = document.getElementById("previous-arrow");
+
+if(document.getElementById("current-page-comic")) {
+  var currentComic = document.getElementById("current-page-comic").getAttribute("alt").slice(-3);
+  var nextComic = (Number(currentComic) + 1).toString().padStart(3, '0');
+  var previousComic = (Number(currentComic) - 1).toString().padStart(3, '0');
+}
 
 document.addEventListener("DOMContentLoaded", function() {
   if (footerCopyright) {
@@ -36,4 +51,36 @@ document.addEventListener("DOMContentLoaded", function() {
     tipJarLink.innerHTML = "TIP THE ARTIST";
     tipJarLink.href = "#TEST2";
   }
+
+  if(nextArrow) {
+    if(nextComic != latestComic) {
+      nextArrow.setAttribute("href", "../BR/" + nextComic + ".html");      
+    } else {
+      nextArrow.setAttribute("href", "../index.html");
+    }
+  }
+
+  if(previousArrow) {
+    if(latestComic == currentComic) {
+      previousArrow.setAttribute("href", "./BR/" + previousComic + ".html");      
+    } else if(previousComic == "000") {
+      previousArrow.setAttribute("href", "./001.html");
+    } else {
+      previousArrow.setAttribute("href", "./" + previousComic + ".html")
+    }
+  }
+
+  // Hide unneeded arrows
+  if(window.location.href.includes("index")) {
+    rightArrows.forEach(arrow => {
+      arrow.classList.add("hidden");
+    });
+  } 
+
+  if(window.location.href.includes("001")) {
+    leftArrows.forEach(arrow => {
+      arrow.classList.add("hidden");
+    });
+  } 
+
 });
